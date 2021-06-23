6be284bda402113de94d99d6470798ea9b5a1069
Devan-Kerman <dev.sel20@gmail.com>
2021-06-23 10:34:08 -0500

    config

---
+++ src/main/java/net/devtech/trident_return/TridentReturnConfig.java
@@ -0,0 +1,22 @@
+package net.devtech.trident_return;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
+
+@Config(name = "trident_return")
+public class TridentReturnConfig implements ConfigData {
+	public boolean enabled = true;
+	@Comment("Anticheat Bypass (warning may delay item moving)")
+	public AntiCheatBypass anticheatBypass = AntiCheatBypass.NONE;
+
+	public enum AntiCheatBypass {
+		NONE,
+		BASIC,
+		ADVANCED;
+
+		public boolean enabled() {
+			return this != NONE;
+		}
+	}
+}
